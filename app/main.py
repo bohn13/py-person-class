86e9ff691eaa95f6f5ecbf9ae5b7387e317479ff
bohn13 <bod3171@gmail.com>
2026-01-20 13:21:50 +0100

    Completed task & checked flake8 before

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
+
 class Person:
-    # write your code here
-    pass
+
+    people = {}
+
+    def __init__(self, name: str, age: int) -> None:
+        self.name = name
+        self.age = age
+        Person.people[self.name] = self
 
 
 def create_person_list(people: list) -> list:
-    # write your code here
-    pass
+    result = []
+    for peep in people:
+        result.append(Person(peep["name"], peep["age"]))
+    for peep in people:
+        person = Person.people[peep["name"]]
+        if "wife" in peep and peep["wife"] is not None:
+            person.wife = Person.people[peep["wife"]]
+        if "husband" in peep and peep["husband"] is not None:
+            person.husband = Person.people[peep["husband"]]
+
+    return result
